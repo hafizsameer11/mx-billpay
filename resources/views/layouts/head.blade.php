f4a601a945523b74ffd202302da5549ec5f1bdce
unknown <hmstech11@gmail.com>
2024-10-14 16:15:56 +0500

    admin panel added

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,24 @@
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>MX Bill Pay || Admin Panel</title>
+  <script src="assets/js/color-modes.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/core/core.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/flatpickr/flatpickr.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/demo1/style.css') }}">
+  <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}">
+
+  <link rel="stylesheet" href="{{ asset('assets/vendors/select2/select2.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/jquery-tags-input/jquery.tagsinput.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/dropzone/dropzone.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/dropify/dist/dropify.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/pickr/themes/classic.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/font-awesome/css/font-awesome.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css') }}">
+
+
---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,34 @@
+<script src="{{ asset('assets/vendors/core/core.js') }}"></script>
+<script src="{{ asset('assets/vendors/flatpickr/flatpickr.min.js') }}"></script>
+<script src="{{asset('assets/vendors/apexcharts/apexcharts.min.js')}}"></script>
+<script src="{{ asset('assets/vendors/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('assets/js/app.js') }}"></script>
+<script src="{{ asset('assets/js/dashboard.js') }}"></script>
+<script src="{{ asset('assets/vendors/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/datatables.net/dataTables.js') }}"></script>
+<script src="{{ asset('assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js') }}"></script>
+
+
+
+<script src="{{ asset('assets/js/form-validation.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap-maxlength.js') }}"></script>
+<script src="{{ asset('assets/js/inputmask.js') }}"></script>
+<script src="{{ asset('assets/js/select2.js') }}"></script>
+<script src="{{ asset('assets/js/typeahead.js') }}"></script>
+<script src="{{ asset('assets/js/tags-input.js') }}"></script>
+<script src="{{ asset('assets/js/dropzone.js') }}"></script>
+<script src="{{ asset('assets/js/dropify.js') }}"></script>
+<script src="{{ asset('assets/js/pickr.js') }}"></script>
+<script src="{{ asset('assets/js/flatpickr.js') }}"></script>
+<script src="{{ asset('assets/vendors/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/jquery-validation/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/bootstrap-maxlength/bootstrap-maxlength.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/inputmask/jquery.inputmask.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/select2/select2.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/typeahead.js/typeahead.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/jquery-tags-input/jquery.tagsinput.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/dropzone/dropzone-min.js') }}"></script>
+<script src="{{ asset('assets/vendors/dropify/dist/dropify.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/pickr/pickr.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/moment/moment.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/flatpickr/flatpickr.min.js') }}"></script>
