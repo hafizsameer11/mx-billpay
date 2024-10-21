920f658a407c57104ffd5899dfa463a7abcb86fe
unknown <hmstech11@gmail.com>
2024-10-21 21:02:46 +0500

    auth token updated

--- app/Http/Controllers/Api/AccountController.php
+++ app/Http/Controllers/Api/AccountController.php
@@ -60,7 +60,7 @@ class AccountController extends Controller
 
         if ($response->successful()) {
 
-            if($responseData['data']=="00"){
+            if($responseData['status']=="00"){
                 $accountData = $response->json();
                 $account = new Account();
                 $account->user_id = $request->userId;
