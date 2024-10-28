943344bed5de15b98127029e1f5310f187722b12
unknown <hmstech11@gmail.com>
2024-10-28 14:19:05 +0500

    new update

--- app/Http/Controllers/Api/UserDetailController.php
+++ app/Http/Controllers/Api/UserDetailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Account;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -21,12 +22,34 @@ class UserDetailController extends Controller
             return response()->json(['message' => $errorMessage, 'status' =>
             'error'], 400);
         }
-        $id=$request->userId;
-        $user=User::where('id',$id)->with('account')->first();
+        $id = $request->userId;
+        $user = User::where('id', $id)->with('account')->first();
+        $userId = $user->id;
+        $email = $user->email;
+        // $firstName = $user->account->firstName;
+        $account = Account::where('user_id', $id)->first();
+        $firstName = $account->firstName;
+        $lastName = $account->lastName;
+        $profilePic = $account->profile_picture;
+        $accountNo = $account->account_number;
+        $balance = $account->accountBalance;
         if (!$user) {
-            return response()->json(['message' => 'User not found', 'status' => 'error'],404);
-        }else{
-            return response()->json(['message' => 'User found', 'status' => 'success','data'=>$user],200);
+            return response()->json(['message' => 'User not found', 'status' => 'error'], 404);
+        } else {
+            return response()->json([
+                'message' => 'User found',
+                'status' => 'success',
+                'data' => [
+                    'userId' => $userId,
+                    'email' => $email,
+                    'firstName' => $firstName,
+                    'lastName' => $lastName,
+                    'profilePic' => $profilePic,
+                    'accountNo' => $accountNo,
+                    'balance' => $balance,
+                ]
+
+            ], 200);
         }
     }
 }
