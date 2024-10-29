017fbc7a297786e3172c73f3239e8965dddf5ae9
unknown <hmstech11@gmail.com>
2024-10-29 18:50:14 +0500

    new table added

--- database/migrations/2024_10_29_130308_create_transactions_table.php
+++ database/migrations/2024_10_29_130308_create_transactions_table.php
@@ -13,19 +13,12 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('transaction_title')->nullable();
-            $table->string('transaction_type')->nullable();
-            $table->string('reference')->nullable();
-            $table->decimal('amount', 15, 2)->nullable(); // Amount of the transaction
-            $table->string('currency')->default('NGN');
-            $table->string('from_account_number')->nullable();
-            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null'); // Foreign key referencing users table
-            $table->string('to_account_title')->nullable();
-            $table->string('from_account_title')->nullable();
-            $table->string('to_account_number')->nullable();
-            $table->string('status')->nullable();
-            $table->timestamp('transaction_date')->nullable(); // Timestamp of when the transaction was processed
-            $table->text('response_message')->nullable(); // Message from the response, useful for errors
+            $table->decimal('amount', 15, 2); // Amount of the transaction
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // Foreign key referencing users table
+            $table->string('transaction_type'); // e.g., 'bill_payme
+            $table->timestamp('transaction_date')->nullable();
+            $table->string('sign')->nullable();
+            $table->string('status')->default('pending');
             $table->timestamps(); // Created at and updated at timestamps
         });
     }
---
+++ database/migrations/2024_10_29_134107_create_transfers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id')->constrained('transactions')->onDelete('cascade'); // Foreign key referencing transactions table
+            $table->string('from_account_number')->nullable(); // Sender's account number
+            $table->string('to_account_number')->nullable(); // Receiver's account number
+            $table->string('from_client_id')->nullable(); // Client ID of the sender
+            $table->string('to_client_id')->nullable(); // Client ID of the receiver
+            $table->string('status')->default('pending'); // Status of the transfer (e.g., pending, completed, failed)
+            $table->string('to_client_name')->nullable();
+            $table->string('from_client_name')->nullable();
+            $table->decimal('amount', 15, 2)->nullable(); // Amount of the transfer
+
+            $table->text('response_message')->nullable(); // Message from the response, useful for errors
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transfers');
+    }
+};
