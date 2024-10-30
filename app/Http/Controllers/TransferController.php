ef7661f59f94b78f858b2ba2361b61a88cb84c88
unknown <hmstech11@gmail.com>
2024-10-30 15:52:07 +0500

    new meessage

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Events\AccountReleased;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TransferController extends Controller
 {
-  public function dispatchevent(){
-    event(new AccountReleased(1));
-  }
+    public function dispatchevent(){
+        event(new AccountReleased(1));
+        Log::info("AccountReleased event dispatched for user 1.");
+    }
+
 }
--- resources/views/account-release-test.blade.php
+++ resources/views/account-release-test.blade.php
@@ -1,27 +1,24 @@
 <!DOCTYPE html>
 <head>
-  <title>Pusher Test</title>
-  <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
-  <script>
+    <title>Pusher Test</title>
+    <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
+    <script>
+        // Enable Pusher logging - don't include this in production
+        Pusher.logToConsole = true;
 
-    // Enable pusher logging - don't include this in production
-    Pusher.logToConsole = true;
+        var pusher = new Pusher('0ad9e7f7e71dc1522ad6', {
+            cluster: 'ap1'
+        });
 
-    var pusher = new Pusher('0ad9e7f7e71dc1522ad6', {
-      cluster: 'ap1'
-    });
-    var userId=1;
+        var userId = 1;
+        var channel = pusher.subscribe(`user.${userId}`);
 
-    var channel = pusher.subscribe(`userid.${userId}`);
-    channel.bind('account.released', function(data) {
-      alert(JSON.stringify(data));
-    });
-  </script>
+        channel.bind('account.released', function(data) {
+            alert("Event received: " + data.message);
+        });
+    </script>
 </head>
 <body>
-  <h1>Pusher Test</h1>
-  <p>
-    Try publishing an event to channel <code>my-channel</code>
-    with event name <code>my-event</code>.
-  </p>
+    <h1>Pusher Test</h1>
+    <p>Waiting for the Account Released event...</p>
 </body>
