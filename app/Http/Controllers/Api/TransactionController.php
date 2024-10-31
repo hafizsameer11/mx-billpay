913043d687a71a8f40d35698b5f64c6eb5f59250
unknown <hmstech11@gmail.com>
2024-10-31 22:05:56 +0500

    new commit

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -61,13 +61,64 @@ class TransactionController extends Controller
             if (!$user) {
                 return response()->json(['status' => 'error', 'message' => 'User not authenticated'], 401);
             }
+
             $billpayments = Transaction::where('user_id', $user->id)
                 ->has('billpayment') // Only get transactions with non-null billpayments
                 ->with(['billpayment.billerItem']) // Eager load billpayment and their related billerItem
-                ->get();
-            if (!$billpayments) {
+                ->get()
+                ->map(function ($transaction) {
+                    return [
+                        'id' => $transaction->id,
+                        'amount' => $transaction->amount,
+                        'user_id' => $transaction->user_id,
+                        'transaction_type' => $transaction->transaction_type,
+                        'transaction_date' => $transaction->transaction_date,
+                        'refference' => $transaction->billpayment->refference,
+                        'customerId' => $transaction->billpayment->customerId,
+                        'sign' => $transaction->sign,
+                        'status' => $transaction->status,
+                        'category' => $transaction->billpayment->billerItem->category->category,
+
+                        'paymentitemname' => $transaction->billpayment->billerItem->paymentitemname,
+                        'billerType' => $transaction->billpayment->billerItem->billerType,
+                        'payDirectitemCode' => $transaction->billpayment->billerItem->payDirectitemCode,
+                        'currencyCode' => $transaction->billpayment->billerItem->currencyCode,
+                        'division' => $transaction->billpayment->billerItem->division,
+                        'created_at' => $transaction->created_at,
+                        'billerId' => $transaction->billpayment->billerItem->billerId,
+                        // 'billpayment' => [
+                        //     'transaction_id' => $transaction->billpayment->id,
+                        //     'user_id' => $transaction->billpayment->user_id,
+                        //     'status' => $transaction->billpayment->status,
+
+
+                        //     'billPaymenDate' => $transaction->billpayment->created_at,
+                        //     'biller_item' => [
+                        //         'id' => $transaction->billpayment->billerItem->id,
+
+                        //         'paymentCode' => $transaction->billpayment->billerItem->paymentCode,
+                        //         'productId' => $transaction->billpayment->billerItem->productId,
+                        //         'paymentitemid' => $transaction->billpayment->billerItem->paymentitemid,
+                        //         'currencySymbol' => $transaction->billpayment->billerItem->currencySymbol,
+                        //         'isAmountFixed' => $transaction->billpayment->billerItem->isAmountFixed,
+                        //         'itemFee' => $transaction->billpayment->billerItem->itemFee,
+                        //         'itemCurrencySymbol' => $transaction->billpayment->billerItem->itemCurrencySymbol,
+                        //         'pictureId' => $transaction->billpayment->billerItem->pictureId,
+
+                        //         'fixed_commission' => $transaction->billpayment->billerItem->fixed_commission,
+                        //         'percentage_commission' => $transaction->billpayment->billerItem->percentage_commission,
+                        //         'created_at' => $transaction->billpayment->billerItem->created_at,
+                        //         'updated_at' => $transaction->billpayment->billerItem->updated_at,
+
+                        //     ],
+                        // ],
+                    ];
+                });
+
+            if ($billpayments->isEmpty()) {
                 return response()->json(['status' => 'error', 'message' => 'No bill payments found'], 404);
             }
+
             return response()->json(['status' => 'success', 'data' => $billpayments], 200);
         } catch (\Exception $e) {
             return response()->json(['status' => 'error', 'message' => 'Failed to retrieve bill payments', 'error' => $e->getMessage()], 500);
