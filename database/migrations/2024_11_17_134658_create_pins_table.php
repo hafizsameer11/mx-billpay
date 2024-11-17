483c372cf31349ecc6f2a803578940a43d262eb6
unknown <hmstech11@gmail.com>
2024-11-17 21:50:26 +0500

    new mesage

--- database/migrations/2024_11_17_134658_create_pins_table.php
+++ database/migrations/2024_11_17_134658_create_pins_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('pins', function (Blueprint $table) {
             $table->id();
             //forign key with users table
-            $table->foreignId('userId')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('pin')->nullable();
             $table->timestamps();
         });
