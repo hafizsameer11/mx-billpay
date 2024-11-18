46ae8b89920a6ac8187ea80166693aca2bf1afa0
unknown <hmstech11@gmail.com>
2024-11-18 11:31:05 +0500

    new

--- app/Jobs/FetchBillerItems.php
+++ app/Jobs/FetchBillerItems.php
@@ -24,7 +24,7 @@ class FetchBillerItems implements ShouldQueue
     {
         $this->categoryName = $categoryName;
 
-        $this->accessToken = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI4MTUiLCJ0b2tlbklkIjoiYzVmOTA4OWMtODAyMS00ZWU3LThjNjYtNTMzMjEwZjQ0NjNkIiwiaWF0IjoxNzI5OTMyMzU2LCJleHAiOjkyMjMzNzIwMzY4NTQ3NzV9.uIQKrplFvnc2ta7RMpwurkoK7guwIbYMBS00NopUxGwUlpP7TC1AqhM1_hns2NEQSw6scWABoeD2PLWpBkgPsA';
+        $this->accessToken = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIzNjgiLCJ0b2tlbklkIjoiMjdiOGNmZjEtYjExNS00MGMzLWIwNDAtMDVjNmViMjljNTk3IiwiaWF0IjoxNzMxOTExNDIyLCJleHAiOjkyMjMzNzIwMzY4NTQ3NzV9.T4dMWHEwST4OUskaQzl1W-dS1rDhFpi7vgPsCfEM58itWKDnbYabnXJnfis4Vr7mIDobzrCaA-UKq9oVjdo49g';
         $this->categoryId = $categoryId; // Set the category ID
     }
 
