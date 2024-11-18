ce9060ebd9333f69a99259ac52565b3ff7f2aed6
unknown <hmstech11@gmail.com>
2024-11-18 18:25:43 +0500

    seeder added

--- database/migrations/2024_11_18_113745_update_account_table.php
+++ database/migrations/2024_11_18_113745_update_account_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Tier;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
@@ -10,15 +11,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    protected $tierId ;
+    public function __construct(){
+        // $this->tierId = DB::table('tier')->max('id'); get first id first id
+        $this->tierId=Tier::first()->id;
+    }
     public function up(): void
     {
         Schema::table('accounts', function (Blueprint $table) {
 
-            $tierId = DB::table('tiers')->first();
-            $tierId=$tierId->id;
 
-            $table->foreignId('tier_id')->constrained()->onDelete('cascade')->default($tierId);
-
+            $table->foreignId('tier_id')->constrained()->onDelete('cascade')->default($this->tierId);
         });
     }
 
@@ -28,11 +31,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('accounts', function (Blueprint $table) {
-//remove foreign key
-$table->dropForeign('accounts_tier_id_foreign');
-$table->dropColumn('tier_id');
-
-
+            //remove foreign key
+            $table->dropForeign('accounts_tier_id_foreign');
+            $table->dropColumn('tier_id');
         });
     }
 };
