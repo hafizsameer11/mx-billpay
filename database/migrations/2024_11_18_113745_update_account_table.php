31650af985fdf7b8640cc02227fb5b0a04e2f512
unknown <hmstech11@gmail.com>
2024-11-18 16:39:38 +0500

    seeder added

---
+++ database/migrations/2024_11_18_113745_update_account_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            //forign key with tiers
+            $table->foreignId('tier_id')->constrained()->onDelete('cascade')->default(1);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            //
+        });
+    }
+};
