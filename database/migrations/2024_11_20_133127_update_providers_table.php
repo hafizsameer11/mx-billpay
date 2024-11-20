56ec634613cc1d1d38f1d12acece6c2ce9a204f2
unknown <hmstech11@gmail.com>
2024-11-20 18:38:20 +0500

    new

--- database/migrations/2024_11_20_133127_update_providers_table.php
+++ database/migrations/2024_11_20_133127_update_providers_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Tier;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -9,11 +10,28 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    protected $tierId;
+    public function __construct()
+    {
+        // $this->tierId = DB::table('tier')->max('id'); get first id first id
+        $this->tierId = Tier::first()->id;
+    }
     public function up(): void
     {
         Schema::table('bill_providers', function (Blueprint $table) {
             $table->unsignedBigInteger('biller_category_id');
             $table->foreign('biller_category_id')->references('id')->on('biller_categories')->onDelete('cascade');
+        });
+        Schema::table('accounts', function (Blueprint $table) {
+
+
+            $table->unsignedBigInteger('tier_id')->default($this->tierId); // Set default value
+            $table->foreign('tier_id')->references('id')->on('tiers')->onDelete('cascade'); // Add foreign key constraint
+
+        });
+        Schema::table('kyc_documents', function (Blueprint $table) {
+
+            $table->string('title')->nullable();
         });
     }
 
