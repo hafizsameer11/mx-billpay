050be4f5560b462396927ffedfb90d55c81d2a5a
unknown <hmstech11@gmail.com>
2024-11-28 15:31:21 +0500

    new

--- app/Http/Controllers/Api/VirtualAccountController.php
+++ app/Http/Controllers/Api/VirtualAccountController.php
@@ -30,7 +30,7 @@ class VirtualAccountController extends Controller
                 'message' => 'Account Already Valid',
                 'data' => [
                     'accountNumber' => $virtualAccount->accountNumber,
-                    'expiryDate' => strtotime($virtualAccount->expiryDate),
+                    'expiryDate' => Carbon::createFromTimestamp(strtotime($virtualAccount->expiryDate)),
                 ]
             ], 200);
         }
