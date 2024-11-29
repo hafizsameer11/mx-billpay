af7b1bba4cd279a30c8786cc47e9e191b75cbbb8
unknown <hmstech11@gmail.com>
2024-11-29 18:54:15 +0500

    new

--- app/Http/Controllers/Api/BillPaymentController.php
+++ app/Http/Controllers/Api/BillPaymentController.php
@@ -211,7 +211,7 @@ class BillPaymentController extends Controller
             $wallet->save();
             $data=[
                 'status'=>'success',
-                'amount'=>$amount,
+                'amount'=>floatval($amount),
                 'item'=>$billerItem->billerId,
                 'provider'=>$billerItem->provider_name,
                 'category'=>$category->category,
