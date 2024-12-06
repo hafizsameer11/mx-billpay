d5ea5d1640c26299d025ab47a6106e42b84be361
unknown <hmstech11@gmail.com>
2024-12-07 03:07:51 +0500

    new messages

--- app/Http/Controllers/Api/UserDetailController.php
+++ app/Http/Controllers/Api/UserDetailController.php
@@ -132,7 +132,9 @@ class UserDetailController extends Controller
         'data' => [
             'firstName' => $account->firstName,
             'lastName' => $account->lastName,
-            'dob' => $account->dob,
+            //make dob to send only date not time
+            'dob' => $account->dob->format('Y-m-d'),
+            // 'dob' => $account->dob,
             'occupation' => $account->occupation,
             'gender' => $account->gender,
             'email' => $account->user->email ?? null, // from related User model
